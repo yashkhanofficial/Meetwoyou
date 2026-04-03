d7124bc214161e86f07670b6ac54bbba7037b03c
yashkhanofficial <info.yashkhan@gmail.com>
2026-04-03 16:05:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,16 @@
-const CACHE_NAME = 'meetwoyou-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;800&display=swap'
-];
+const CACHE_NAME = 'meetwoyou-v5';
+const assets = ['./', './index.html'];
 
-// Install SW
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  self.skipWaiting();
 });
 
-// Cache and return requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
